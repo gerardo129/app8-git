fda71afeed1eb6a48934b6b96abc82fd91197ebe
gerardo129 <Hrhunter17x@gmail.com>
2017-05-19 13:28:31 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -42,7 +42,7 @@ $('#vibrar').tap(function(){
 $(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
-		$('#disp table td').eq(5).text(device.cordava);
+		$('#disp table td').eq(5).text(device.cordova);
 		$('#disp table td').eq(7).text(device.platform);
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
@@ -64,7 +64,7 @@ $(document).ready(function (e){
 });//document
 
 function escribehistoria(accion){
-	$('#ehistoria').append('<li>'+accion+'</li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
 			
 			
